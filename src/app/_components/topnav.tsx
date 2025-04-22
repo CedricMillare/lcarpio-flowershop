d3a15f4ba52887439be786ad20b4d906094c790f
CedriMillare <cmamillare.student@ua.edu.ph>
2025-04-22 21:08:07 +0800

    initial commit

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,21 +1,25 @@
 export function TopNav() {
     return (
-            <nav className="max-w p-4 flex items-center justify-between">
+            <nav className="max-w flex items-center justify-between p-4">
                 <div className="text-xl font-bold">Lcarpio's Flower Shop</div>
                 
                 <div className="flex gap-5 list-none class">
-                    <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Home</a>
-                    <li className="relative group">
+                    <div className="relative group">
+                        <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Home</a>
+                    </div>
+                    <div className="relative group">
                         <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Bouquet</a>
-                        <ul className="absolute items-center pt-5 hidden group-hover:block bg-white text-black rounded">
+                        <ul className="fixed pt-5 hidden group-hover:block bg-white text-black rounded">
                             <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Small Bouquet</a></li>
                             <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Big Bouquet</a></li>
                             <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Funeral</a></li>
                             <li><a href="#" className="block px-4 py-2 hover:bg-gray-100">Wedding</a></li>
                         </ul>
-                    </li>
-                    <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Flowers</a>
-                    <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Orders</a>
+                    </div>
+                    <div className="relative group">
+                        <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Flowers</a>
+                        <a href="#" className=" text-white px-4 py-2 rounded hover:bg-gray-500">Orders</a>
+                    </div>
                 </div>
 
                 <div>
